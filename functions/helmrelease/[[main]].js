d4a782891c385ec2efdaf77b9c0222dd4d258486
JJGadgets <git@jjgadgets.tech>
2024-12-18 12:21:08 +0800

    fix(helmrelease): improve URL handling

--- functions/helmrelease/[[main]].js
+++ functions/helmrelease/[[main]].js
@@ -5,13 +5,13 @@ export async function onRequest(context) {
   url.pathname = `helmrelease-helm-` + apiVersion + `.json`;
   let valuesFile;
   if (vars[1] === "github") {
-    valuesFile = `https://raw.githubusercontent.com/` + vars.slice(2).join('/');
-    if (valuesFile.endsWith(".json") === false) {valuesFile += `/values.schema.json`};
+    valuesFile = new URL(`https://raw.githubusercontent.com/` + vars.slice(2).join('/'));
+    if (valuesFile.pathname.endsWith(".json") === false) {valuesFile.pathname += `/values.schema.json`};
   } else {
-    valuesFile = vars.slice(1).join('/');
+    valuesFile = new URL(vars.slice(1).join('/'));
   };
   const upstream = await context.env.ASSETS.fetch(url);
   let schema = JSON.parse(JSON.stringify(await upstream.json()));
-  schema.properties.spec.properties.values.$ref = valuesFile;
+  schema.properties.spec.properties.values.$ref = valuesFile.toString();
   return new Response(JSON.stringify(schema), upstream)
 }
