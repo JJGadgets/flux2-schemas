56cb4582d00ad3dcc6d29ce7de06e92a5b24f6af
JJGadgets <git@jjgadgets.tech>
2024-12-18 12:41:27 +0800

    fix(helmrelease): improve URL helpers

--- functions/helmrelease/[[main]].js
+++ functions/helmrelease/[[main]].js
@@ -1,14 +1,21 @@
 export async function onRequest(context) {
   const vars = context.params.main;
   const apiVersion = vars[0];
+  const urlParams = new URLSearchParams(context.url)
   let url = new URL(context.request.url);
   url.pathname = `helmrelease-helm-` + apiVersion + `.json`;
   let valuesFile;
+  function jsonAppend() {
+    if (valuesFile.pathname.endsWith(".json") === false) {valuesFile.pathname += `/values.schema.json`};
+  }
   if (vars[1] === "github") {
     valuesFile = new URL(`https://raw.githubusercontent.com/` + vars.slice(2).join('/'));
-    if (valuesFile.pathname.endsWith(".json") === false) {valuesFile.pathname += `/values.schema.json`};
+    jsonAppend();
+  } else if (vars[1].startsWith("https") === false) {
+    valuesFile = new URL(`https://` + vars.slice(1).join('/'));
+    jsonAppend();
   } else {
-    valuesFile = new URL(vars.slice(1).join('/'));
+    valuesFile = new URL(urlParams.url);
   };
   const upstream = await context.env.ASSETS.fetch(url);
   let schema = JSON.parse(JSON.stringify(await upstream.json()));
