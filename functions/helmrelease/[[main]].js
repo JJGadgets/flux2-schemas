fbab912cf01fbca430a2db9ebea297f7f05c75b2
JJGadgets <git@jjgadgets.tech>
2024-12-18 12:17:37 +0800

    fix(helmrelease): fetch in JSON

--- functions/helmrelease/[[main]].js
+++ functions/helmrelease/[[main]].js
@@ -4,7 +4,7 @@ export async function onRequest(context) {
   let url = new URL(context.request.url);
   url.pathname = `helmrelease-helm-` + apiVersion + `.json`;
   let valuesFile;
-  if (vars[1] = "github") {
+  if (vars[1] === "github") {
     valuesFile = `https://raw.githubusercontent.com/` + vars.slice(2).join('/');
     if (valuesFile.endsWith(".json") === false) {valuesFile += `/values.schema.json`};
   } else {
@@ -12,7 +12,6 @@ export async function onRequest(context) {
   };
   const upstream = await context.env.ASSETS.fetch(url);
   let schema = JSON.parse(JSON.stringify(await upstream.json()));
-  //let schema = upstream.json();
   schema.properties.spec.properties.values.$ref = valuesFile;
   return new Response(JSON.stringify(schema), upstream)
 }
