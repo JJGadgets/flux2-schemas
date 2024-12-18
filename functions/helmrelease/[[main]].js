3ce65d6d952ac8f281bdbf7643ebad15f7a51ba5
JJGadgets <git@jjgadgets.tech>
2024-12-18 11:23:07 +0800

    fix(helmrelease): use proper URL

--- functions/helmrelease/[[main]].js
+++ functions/helmrelease/[[main]].js
@@ -1,15 +1,17 @@
 export async function onRequest(context) {
   const vars = context.params.main;
   const apiVersion = vars[0];
-  var valuesFile;
+  let url = new URL(context.request.url);
+  url.pathname = `helmrelease-helm-` + apiVersion + `.json`;
+  let valuesFile;
   if (vars[1] = "github") {
     valuesFile = `https://raw.githubusercontent.com/` + vars.slice(2).join('/');
     if (valuesFile.endsWith(".json") === false) {valuesFile += `/values.schema.json`};
   } else {
     valuesFile = vars.slice(1).join('/');
   };
-  const upstream = await context.env.ASSETS.fetch(`helmrelease-helm-` + apiVersion + `.json`);
-  var schema = JSON.parse(upstream.body);
+  const upstream = await context.env.ASSETS.fetch(url);
+  let schema = JSON.parse(upstream.body);
   schema.properties.spec.properties.values.$ref = valuesFile;
   return new Response(JSON.stringify(schema), upstream)
 }
