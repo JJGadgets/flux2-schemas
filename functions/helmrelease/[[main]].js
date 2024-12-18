1f95171011362a551f19abf8b6c17745d1bf6a97
JJGadgets <git@jjgadgets.tech>
2024-12-18 11:28:26 +0800

    fix(helmrelease): fetch in JSON

--- functions/helmrelease/[[main]].js
+++ functions/helmrelease/[[main]].js
@@ -11,7 +11,7 @@ export async function onRequest(context) {
     valuesFile = vars.slice(1).join('/');
   };
   const upstream = await context.env.ASSETS.fetch(url);
-  let schema = JSON.parse(upstream.body);
+  let schema = JSON.parse(upstream.json());
   schema.properties.spec.properties.values.$ref = valuesFile;
   return new Response(JSON.stringify(schema), upstream)
 }
